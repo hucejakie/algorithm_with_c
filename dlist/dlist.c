a626a921e91beb7a7f40c0d57d779dd6d04b960b
Jakie <hucejakie@126.com>
2018-03-28 15:13:11 +0800

    dlist

--- dlist/dlist.c
+++ dlist/dlist.c
@@ -29,45 +29,133 @@ void dlist_destroy(DList *list)
 }
 
 
-//int dlist_ins_next(DList *dlist,DListElmt *element,const void *data)
-//{
-//	return 0;
-//}
-//
-//
-//int dlist_ins_prev(DList *dlist,DListElmt *element,const void *data)
-//{
-//	return 0;
-//}
-//
-//
-int dlist_remove(DList *list,DListElmt *element,const void **data)
+int dlist_ins_next(DList *list,DListElmt *element,const void *data)
 {
-	if(element==NULL || dlist_size(list)==0)
+	DListElmt *new_element;
+
+	 /// 除非是空双向链表，否则不允许 element 为 NULL
+	if(dlist_size(list)!=0 && element==NULL)
 		return -1;
 
-	*data=element->data;
-	if(element=list->head)
+	if ((new_element=(DListElmt *)malloc(sizeof(DListElmt)))==NULL)
+		return -1;
+
+	new_element->data=(void *)data;
+
+	if(dlist_size(list)==0)
 	{
-		list->head=element->next;
-		if(list->head==NULL)
-			list->tail=NULL;
-		else
-		element->next->prev=NULL;
+		list->head=new_element;
+		list->tail=new_element;
+		new_element->prev=NULL;
+		new_element->next=NULL;
 	}
 	else
 	{
-		element->prev->next=element->next;
-		if(element->next==NULL)
-		{
-			list->tail=element->prev;
-		}
-		else
-			element->next->prev=element->prev;	
-	}
-	free(element);
-	list->size--;
+		new_element->prev=element;
+		new_element->next=element->next;
 
+		if(element->next==NULL)
+		{ list->tail=new_element;}
+		else
+		{
+		element->next->prev=new_element;
+		}
+
+		element->next=new_element;
+	
+	}
+	list->size++;
 	return 0;
 }
-//
+
+
+int dlist_ins_prev(DList *list,DListElmt *element,const void *data)
+{
+	DListElmt * new_element;
+	if( element==NULL && dlist_size(list)!=0 )
+		return -1;
+
+	if ((new_element=(DListElmt *)malloc(sizeof(DListElmt)))==NULL)
+		return -1;
+
+	new_element->data=(void *)data;
+
+	if(dlist_size(list)==0)
+	{
+		list->head=new_element;
+		list->head->prev=NULL;
+		list->head->next=NULL;
+		list->tail=new_element;
+	
+	}
+	else
+	{
+		new_element->next=element;
+		new_element->prev=element->prev;
+		
+
+		if(element->prev==NULL)
+		{ 
+			list->head=new_element;
+		}
+		else
+		{
+			element->prev->next=new_element;
+		}
+
+		element->prev=new_element;
+	
+	}
+	list->size++;
+	return 0;
+}
+
+
+
+int dlist_remove(DList *list, DListElmt *element, void **data)
+{
+
+	
+
+  /// 禁止删除 element 为空或空链表的数据
+  if (element == NULL || dlist_size(list) == 0) return -1;
+
+  *data = element->data;
+  
+  if (element == list->head) {
+    
+    /// 处理删除头部元素
+    
+    list->head = element->next;
+    
+    if (list->head == NULL) {
+      list->tail = NULL;
+    } else {
+      element->next->prev = NULL;
+    }
+  } else {
+    
+    /// 处理删除非头部元素
+    
+    element->prev->next = element->next;
+    
+    if (element->next == NULL) {
+      list->tail = element->prev;
+    } else {
+      element->next->prev = element->prev;
+    }
+  }
+  
+  /// 清理旧结点元素的
+  free(element);
+  
+  /// 更新链表的 size
+  list->size--;
+  
+  return 0;
+}
+
+
+
+
+
--- dlist/main.c
+++ dlist/main.c
@@ -2,7 +2,54 @@
 #include <stdio.h>
 #include "dlist.h"
 
+void print_list(DList *list)
+{
+	DListElmt *element;
+    int	* data,i;
+	fprintf(stdout,"-> Dlist size is %d\n",dlist_size(list));
+	i=0;
+	element=dlist_head(list);
+	while(1)
+	{
+	 data=dlist_data(element);
+	 fprintf(stdout,"-> List[%03d] = %03d\n",i,*data);
+	 
+	 i++;
+
+	 if(dlist_is_tail(element))
+	 { 
+		 break;
+	 }
+	 else
+	 {
+		 element=dlist_next(element);
+	 }
+
+	}
+	return;
+}
 int main(int argc, const char * argv[])
 {
+	DList list;
+	DListElmt *element;
+	int *data,i;
+	dlist_init(&list,free);
+
+	element=dlist_head(&list);
+	for(i=10;i>0;i--)
+	{
+		if((data=(int*)malloc(sizeof(int)))==NULL)
+			return -1;
+		*data=i;
+
+		if(dlist_ins_prev(&list,dlist_head(&list),data)!=0) 
+			return 1;
+	
+	}
+	print_list(&list);
+
+	dlist_destroy(&list);
+
+	system("PAUSE");
 	return 0;
 }
