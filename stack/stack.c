cd4055baaa1d9aa8bc97938e166f24077e0dfe4f
Jakie <hucejakie@126.com>
2018-04-02 17:18:30 +0800

    add stack and queue

---
+++ stack/stack.c
@@ -0,0 +1,19 @@
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#include"stack.h"
+#include "list.h"
+
+int stack_push(Stack *stack,const void *data)
+{
+ return list_ins_next(stack,NULL,data);
+}
+
+int stack_pop(Stack *stack, void **data)
+{
+ return list_rem_next(stack,NULL,data);
+}
+
+
+
